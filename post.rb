8517cde2bdd22fd97c767ce4bb9fd6e10af8defe
Inga <guingg@inbox.ru>
2018-08-21 09:45:57 -0700

    second

---
+++ post.rb
@@ -0,0 +1,36 @@
+class Post
+
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strngs
+
+
+  end
+
+  def save
+    f = File.new(file_path, "w:UTR-8")
+    for item in to_strings do
+      f.puts(item)
+    end
+    f.close
+
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+
+    return current_path + "/" + file_name
+  end
+
+
+
+end
